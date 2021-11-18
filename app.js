4dc982958449c613f2468273c32d2c15784a03db
Alejandro Estrada <alejandro.estradam@udea.edu.co>
2021-11-17 20:26:53 -0500

    demo inicial de ts

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+let msg = 'hola mundo';
+const mensaje = 'Hola Mundo!!!';
+const hero = {
+    name: 'Ironman',
+    age: 15
+};
+hero.age = 50; // js transpila pero confiando en las habilidadesd el desarrollador
+console.log(hero);
---
+++ app.ts
@@ -0,0 +1,14 @@
+
+
+let msg = 'hola mundo';
+const mensaje: string = 'Hola Mundo!!!'
+
+
+const hero = {
+    name : 'Ironman',
+    age : 15
+}
+
+hero.age = 50 // js transpila pero confiando en las habilidadesd el desarrollador
+
+console.log(hero)
